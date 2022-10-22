minor fixes; correct gt_flow calculation

eval.py
@@ -48,10 +48,11 @@ def main():
     model = RecursiveCascadeNetwork(n_cascades=args.n_cascades, im_size=image_size).cuda()
     # add checkpoint loading
     print("Loading checkpoint from {}".format(args.checkpoint))
+    from tools.utils import load_model, load_model_from_dir
+    model_path = args.checkpoint
     if os.path.isdir(args.checkpoint):
-        load_model_from_dir(args.checkpoint, model)
+        model_path = load_model_from_dir(args.checkpoint, model)
     else:
-        model_path = args.checkpoint
         load_model(torch.load(model_path), model)
     
     # parent of model path
@@ -109,9 +110,10 @@ def main():
             print("{}: {} ({})".format(dice_k, np.mean(results[dice_k]), np.std(results[dice_k])), file=fo)
 
     if args.save_pkl:
-        with open(output_fname.replace('.txt', '.pkl'), 'wb') as fo:
+        pkl_name = output_fname.replace('.txt', '.pkl')
+        with open(pkl_name, 'wb') as fo:
             pickle.dump(results, fo)
-        print('finish saving pkl')
+        print('finish saving pkl to {}'.format(pkl_name))
 
 if __name__ == '__main__':
     main()

tools/analysis.py
@@ -9,9 +9,9 @@ import sys
 sys.path.append('..')
 sys.path.append('.')
 from networks.spatial_transformer import SpatialTransform
-from utils import draw_seg_on_vol, show_img, visualize_3d
+from utils import draw_seg_on_vol, show_img, visualize_3d, combo_imgs
 import torch
-from thres_ffd import FFD_GT
+from thres_ffd import FFD_GT, cal_single_warped
 
 
 def eP2P(flow, gt, mask):
@@ -27,76 +27,128 @@ def cal_warped_seg(agg_flows, seg2):
     w_segs = st(seg2, agg_flows, mode='nearest')
     return w_segs
 
-def cal_single_warped(flow, img):
-    st = SpatialTransform(img.shape)
-    w_img = st(img[None,None], flow[None], mode='bilinear')
-    return w_img[0,0]
-#%%
-fname = '/home/hynx/regis/recursive-cascaded-networks/evaluations/Sep27_145203_normal_lits_d_.pkl'
-mode = 'normal'
-# fname = '/home/hynx/regis/recursive-cascaded-networks/evaluations/Oct03_210121_masked_lits_d_.pkl'
-# mode = 'mask'
+def single_jacobian_det(flow):
+    """
+    flow has shape (batch, C, H, W, S)
+    """
+    flow = flow[None]
+    # Compute Jacobian determinant
+    batch_size, _, height, width, depth = flow.shape
+    dx = flow[:, :, 1:, 1:, 1:] - flow[:, :, :-1, 1:, 1:] + 1
+    dy = flow[:, :, 1:, 1:, 1:] - flow[:, :, 1:, :-1, 1:] + 1
+    dz = flow[:, :, 1:, 1:, 1:] - flow[:, :, 1:, 1:, :-1] + 1
+    jac = np.stack([dx, dy, dz], axis=1).transpose(0, 3, 4, 5, 1, 2)
+    det = np.linalg.det(jac)
+    return det[0]
 
-dct = pkl.load(open(fname, 'rb'))
-print(dct.keys())
-print(dct['dice_liver'])
-#%%
-idx = 0
-errs = []
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    #%%
+    fname = '/home/hynx/regis/recursive-cascaded-networks/evaluations/Sep27_145203_normal_lits_d_.pkl'
+    mode = 'normal'
+    fname = '/home/hynx/regis/recursive-cascaded-networks/evaluations/Oct03_210121_masked_lits_d_.pkl'
+    mode = 'mask'
+    # fname = '/home/hynx/regis/recursive-cascaded-networks/evaluations/Oct11_183942_soft-m_lits_d_.pkl'
+    # mode = 'soft_mask'
+    # fname = '/home/hynx/regis/recursive-cascaded-networks/evaluations/Oct16_164126_normal_o.01_lits_d_.pkl'
+    # mode = 'normal_o.01'
 
-h5_name = '/home/hynx/regis/recursive-cascaded-networks/datasets/lits_deform_L.h5'
-reader = FFD_GT(h5_name)
+    dct = pkl.load(open(fname, 'rb'))
+    print(dct.keys())
+    print(dct['dice_liver'])
+    #%%
+    idx = 0
+    errs = []
 
-save_im = False
-for idx in range(len(dct['id1'])):
-    agg_flow = dct['agg_flows'][idx][-1].permute(1,2,3,0).numpy()
-    
-    id1 = dct['id1'][idx][7:]
-    id2 = dct['id2'][idx]
-    print('analyzing', id1, id2)
-    reader.set_to(id1, id2);
-    gt = reader.ffd_gt
-    gt_flow = gt - np.mgrid[0:gt.shape[0], 0:gt.shape[1], 0:gt.shape[2]].astype(np.float32).transpose(1,2,3,0)
-    seg2 = reader.seg2
-    seg1 = reader.seg1
+    h5_name = '/home/hynx/regis/recursive-cascaded-networks/datasets/lits_deform_fL.h5'
+    reader = FFD_GT(h5_name)
 
-    w_segs= cal_warped_seg(dct['agg_flows'][idx], torch.tensor(seg2)[None,None].repeat(4,1,1,1,1).float())
-    for i, s in enumerate([seg2, *w_segs, seg1]):
-        print('{}th layer| organ size {}| tumor size {}'.format(i, (s>0.5).sum(), (s>1.5).sum()))
+    save_im = True
+    for idx in range(len(dct['id1'])):
+    # for idx in [0]:
+        agg_flow = dct['agg_flows'][idx][-1].permute(1,2,3,0).numpy()
+        
+        id1 = dct['id1'][idx][7:]
+        id2 = dct['id2'][idx]
+        print('analyzing', id1, id2)
+        reader.set_to(id1, id2);
+        gt_flow = reader.gt_flow
+        seg2 = reader.seg2
+        seg1 = reader.seg1
 
-    masks = [seg2>1.5]
-    thres = np.percentile(reader.disp, [85, 95, 99])
-    masks = masks + [np.logical_or(seg2>1.5, m) for m in reader.get_masks(thres)]
-    err = [eP2P(agg_flow, gt_flow, m) for m in masks]
-    print('{} to {} flow end to end error:'.format(id1, id2), err)
-    errs.append(err)
+        w_segs= cal_warped_seg(dct['agg_flows'][idx], torch.tensor(seg2)[None,None].repeat(4,1,1,1,1).float())
+        for i, s in enumerate([seg2, *w_segs, seg1]):
+            print('{}th layer| organ size {}| tumor size {}'.format(i, (s>0.5).sum(), (s>1.5).sum()))
 
-    # save image
-    if save_im:
-        img1 = reader[id1]['volume'][...]
-        img2 = reader[id1]['id2'][id2]['volume'][...]
-        img_dir = pa('../images/{}/{}_{}'.format(mode, id1, id2))
-        img_dir.mkdir(parents=True, exist_ok=True)
-        show_img(img1).save(img_dir/'img1.png')
-        # draw tumor seg to img2
-        s_img2 = draw_seg_on_vol(img2[None], (seg2==2)[None], colors=[(250,0,0)])
-        show_img(s_img2).save(img_dir/'img2.png')
-        # save warped
-        warped = cal_single_warped(dct['agg_flows'][idx][-1], torch.tensor(img2).float())
-        s_warped = draw_seg_on_vol(warped[None], (w_segs[-1]==2), colors=[(250,0,0)])
-        show_img(s_warped).save(img_dir/'s_warped.png')
-        show_img(warped).save(img_dir/'warped.png')
-        print(img_dir/'warped.png')
-    
-    print()
-print(errs)
-print(mode, 'mean pnp error:', np.mean(errs, axis=0), 'std:', np.std(errs, axis=0))
+        masks = [seg2>1.5]
+        thres = np.percentile(reader.disp, [90, 95, 99])
+        masks = masks + [np.logical_or(seg2>1.5, m) for m in reader.get_masks(thres)]
+        err = [eP2P(agg_flow, gt_flow, m) for m in masks]
+        print('{} to {} flow end to end error:'.format(id1, id2), err)
+        errs.append(err)
 
-#%%
-# np.histogram(reader.disp, bins=5)    
-np.percentile(reader.disp, [85, 95, 99])
-# %%
-from utils import combine_pil_img
-combine_pil_img(*[show_img(im) for im in masks])
-#%% close reader
-reader.close()
+        #%% 
+        areas = abs(single_jacobian_det(agg_flow.transpose(3,0,1,2)))
+        values = np.percentile(areas, [85, 90, 95, 100])
+        q_areas = np.digitize(areas, values)
+        from tools.utils import plt_img3d_axes
+        def func_a(ax, i):
+            # p = ax.imshow(np.exp(areas[i]), cmap='jet', alpha=.5)
+            # plt.colorbar(p, ax=ax)
+            d = ax.contourf(areas[i], levels=values, linewidths=.8)
+            # ax.clabel(d, inline=True, fontsize=5)
+            ax.imshow(reader.img2[i], cmap='gray', alpha=.5)
+
+        #%% check r_ffd (larger disp than p_ffd)
+        r_ffd = reader.r_ffd
+        def ffd_flow(ffd, size):
+            pt = np.mgrid[0:size[0], 0:size[1], 0:size[2]].astype(np.float32).reshape(3, -1).T
+            flow = ffd(pt)-pt
+            flow = flow.T.reshape(3,*size)
+            return flow
+        r_ffd_flow = ffd_flow(r_ffd, seg2.shape)
+        p_ffd_flow = ffd_flow(reader.p_ffd, seg2.shape)
+        #%%
+        # np.histogram(r_ffd_flow)
+        show_img(np.linalg.norm(reader.gt_flow, axis=-1))
+        # show_img(np.linalg.norm(p_ffd_flow, axis=0))
+        # np.histogram(np.linalg.norm(r_ffd_flow, axis=0)),np.histogram(np.linalg.norm(p_ffd_flow, axis=0))
+
+        #%% save image
+        if save_im:
+            img1 = reader.img1
+            img2 = reader.img2
+            img_dir = pa('../images/{}/{}_{}'.format(mode, id1, id2))
+            img_dir.mkdir(parents=True, exist_ok=True)
+            # draw tumor seg to img2
+            s_img2 = draw_seg_on_vol(img2[None], (seg2==2)[None], colors=[(250,0,0)])
+            # save warped
+            warped = cal_single_warped(dct['agg_flows'][idx][-1], torch.tensor(img2).float())
+            s_warped = draw_seg_on_vol(warped[None], (w_segs[-1]==2), colors=[(250,0,0)])
+            combo_imgs(img1, s_img2, s_warped).save(img_dir/'deform.png')
+            print(img_dir/'deform.png')
+            # save thresholded masks
+            combo_imgs(*[m.astype(int)+(seg2>0)+(seg2==2) for m in masks]).save(img_dir/'masks.png')
+            print(img_dir/'masks.png')
+            #%% save eP2P error
+            from tools.utils import plt_img3d_axes
+            p2p_err = agg_flow-gt_flow
+            p2p_err = np.linalg.norm(p2p_err, axis=-1)
+            p2p_err[masks[2]]=0
+            def func(ax, i):
+                ax.imshow(p2p_err[i], cmap='jet', alpha=.5)
+                # d = ax.contour(p2p_err[i], levels=5, linewidths=.8)
+                # ax.clabel(d, inline=True, fontsize=5)
+                ax.imshow(img2[i], cmap='gray', alpha=.5)
+            plt_img3d_axes(p2p_err, func).savefig(img_dir/'p2p_err.png')
+            # show_img(p2p_err).save(img_dir/'p2p_err.png')
+            print(img_dir/'p2p_err.png')
+            plt_img3d_axes(areas, func_a).savefig(img_dir/'areas.png')
+            print(img_dir/'areas.png')
+        #%%
+        print()
+    print(errs)
+    print(mode, 'mean pnp error:', np.mean(errs, axis=0), 'std:', np.std(errs, axis=0))
+
+    #%% close reader
+    reader.close()

tools/thres_ffd.py
@@ -45,8 +45,8 @@ class FFD_GT:
         # get warping gt
         points = self.ffd_gt.reshape(-1,3)
         values = -self.return_flow().reshape(-1,3)
-        xi = np.mgrid[0:img2.shape[0], 0:img2.shape[1], 0:img2.shape[2]].astype(np.float32).reshape(3, -1).T
-        self.gt_flow = griddata(points, values, xi, method='nearest')
+        xi = np.mgrid[0:self.shape[0], 0:self.shape[1], 0:self.shape[2]].astype(np.float32).reshape(3, -1).T
+        self.gt_flow = griddata(points, values, xi, method='nearest').reshape(*self.shape)
         # get threshold
         self.thres = np.percentile(self.disp, 95)
         return self
@@ -86,6 +86,15 @@ class FFD_GT:
     def close(self):
         self.reader.close()
 
+from networks.spatial_transformer import SpatialTransform
+import torch
+def cal_single_warped(flow, img):
+    if not isinstance(img, torch.Tensor):
+        img = torch.tensor(img)
+    img = img.float()
+    st = SpatialTransform(img.shape)
+    w_img = st(img[None,None], flow[None], mode='bilinear')
+    return w_img[0,0]
 #%% main function
 if __name__ == '__main__':
     from utils import show_img, combine_pil_img, combo_imgs, plt_img3d_show
@@ -127,12 +136,7 @@ if __name__ == '__main__':
     import sys
     sys.path.append('..')
     sys.path.append('.')
-    from networks.spatial_transformer import SpatialTransform
-    def cal_single_warped(flow, img):
-        img = torch.tensor(img).double()
-        st = SpatialTransform(img.shape)
-        w_img = st(img[None,None], flow[None], mode='bilinear')
-        return w_img[0,0]
+    
     
     def do_warp(flow, img, seg=None, order=1):
         x, y, z = np.meshgrid(np.arange(img.shape[0]), np.arange(img.shape[1]), np.arange(img.shape[2]), indexing='ij')

tools/utils.py
@@ -18,8 +18,9 @@ def load_model_from_dir(checkpoint_dir, model):
     p = sorted(p, key=lambda x: [int(n) for n in re.findall(r'\d+', str(x))])
     model_path = str(p[-1])
     load_model(torch.load(model_path), model)
+    return model_path
 
-def visualize_3d(data, width=5, inter_dst=5, save_name=None):
+def visualize_3d(data, width=5, inter_dst=5, save_name=None, print_=False):
     """
     data: (S, H, W) or (N, C, H, W)"""
     data =tt(data)
@@ -32,7 +33,8 @@ def visualize_3d(data, width=5, inter_dst=5, save_name=None):
     if img.dim() < 4:
         img = img[:, None]
     img = img[::inter_dst]
-    print("Visualizing img with shape and type:", img.shape, img.dtype, "on path {}".format(save_name) if save_name else "")
+    if print_:
+        print("Visualizing img with shape and type:", img.shape, img.dtype, "on path {}".format(save_name) if save_name else "")
 
     img_f = make_grid(img, nrow=width, padding=5, pad_value=1, normalize=True)
     if save_name:
@@ -133,3 +135,4 @@ def plt_img3d_axes(imgs, func, intrv=5):
     # tight
     plt.tight_layout()
     plt.show()
+    return plt