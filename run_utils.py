f5ae902068bf88571cc4d1ffd119c63bd1c82220
dddraxxx <15207615513@qq.com>
2023-03-30 20:00:29 +0000

    add doc

--- run_utils.py
+++ run_utils.py
@@ -22,26 +22,33 @@ for i in stage1_cfg:
         stage1_cfg[i][j] = os.path.realpath(stage1_cfg[i][j])
 
 def build_precompute(model, dataset, cfg):
+    # get template image and segmentation
     template = list(dataset.subset['temp'].values())[0]
     template_image, template_seg = template['volume'], template['segmentation']
+    # convert to torch tensors and move to GPU
     template_image = torch.tensor(np.array(template_image).astype(np.float32)).unsqueeze(0).unsqueeze(0).cuda()/255.0
     template_seg = torch.tensor(np.array(template_seg).astype(np.float32)).unsqueeze(0).unsqueeze(0).cuda()
+    # load pretrained model
     state_path = stage1_cfg[cfg.data_type][cfg.base_network]
     if cfg.stage1_rev:
         print('using rev_flow')
     return model.build_preregister(template_image, template_seg, state_path, cfg.base_network)
 
 def read_cfg(model_path):
+    # Traverse up the directory tree until we find the folder where the model is saved
     while 'model_wts' in model_path:
         model_path = pa(model_path).parent
+    # Get the model name
     name = pa(model_path).stem.split('_')[-1]
     cfg_path = pa(model_path) / 'args.txt'
-    # read till the first empty line
+    # Read the first line of the file
     cfg = open(cfg_path).read().split('\n\n')[0]
-    # turn single quotes to double quotes
+    # Replace single quotes with double quotes
     cfg = cfg.replace("'", '"')
+    # Convert the string to a dictionary
     cfg = eval(cfg)
     cfg['name'] = name
+    # Convert the dictionary to a ConfigDict
     import ml_collections
     cfg = ml_collections.ConfigDict(cfg)
     print(cfg)
